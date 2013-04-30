c7a7dbfc7f441f08f800a3b8eb92f6f2d8d4f294
Leonardo Rochael Almeida <leorochael@gmail.com>
2013-04-30 18:23:49 -0300

    SubstanceD on Heroku demo app
    Modifications and additions necessary for running on Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: pserve production.ini database_url=$DATABASE_URL http_port=$PORT
--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.txt')).read()
-CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+README = open(os.path.join(here, 'README.rst')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
 
 requires = [
     'pyramid',
@@ -15,8 +15,8 @@ requires = [
     ]
 
 setup(name='substancedemo',
-      version='0.0',
-      description='substancedemo',
+      version='0.1',
+      description='Proof of concept for heroku deployment of SubstanceD',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
         "Programming Language :: Python",
